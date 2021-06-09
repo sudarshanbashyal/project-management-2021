4c3e2b77c9c76fa63f041b4619f20c82e4a89aa5
Sudarshan Bashyal <sudarshanbsyl@gmail.com>
2021-06-09 09:39:00 +0545

    Cart when logged out and bug fixes

--- cart/deleteCart.php
+++ cart/deleteCart.php
@@ -1,19 +1,24 @@
 <?php
 
     include '../init.php';
-    $userId = $_SESSION['userId'];
 
-    $deleteQuery = "
-        DELETE cd.* FROM cart_details cd
-        INNER JOIN cart c ON c.cart_id = cd.cart_id
-        INNER JOIN users u ON u.user_id = c.user_id
-        WHERE u.user_id=$userId;
-    ";
-    $deleteQueryResult = mysqli_query($connection, $deleteQuery);
+    if(isset($_SESSION['userId'])){
+        $userId = $_SESSION['userId'];
+        $deleteQuery = "
+            DELETE cd.* FROM cart_details cd
+            INNER JOIN cart c ON c.cart_id = cd.cart_id
+            INNER JOIN users u ON u.user_id = c.user_id
+            WHERE u.user_id=$userId;
+        ";
 
-    if($deleteQueryResult){
+        $deleteQueryResult = mysqli_query($connection, $deleteQuery);
+        if($deleteQueryResult){
+            header('Location: cart.php');
+        }
+    }
+    else{
+        $_SESSION['currentCart']=array();
         header('Location: cart.php');
     }
-
 
 ?>
--- cart/deleteCartProduct.php
+++ cart/deleteCartProduct.php
@@ -1,20 +1,36 @@
 <?php
 
     include '../init.php';
-    $userId = $_SESSION['userId'];
-
     $productId = $_GET['productId'];
 
-    $deleteQuery = "
-        DELETE cd.* FROM cart_details cd
-        INNER JOIN cart c ON c.cart_id = cd.cart_id
-        INNER JOIN users u ON u.user_id = c.user_id
-        WHERE u.user_id=$userId AND cd.product_id=$productId;
-    ";
-    $deleteQueryResult = mysqli_query($connection, $deleteQuery);
+    if(isset($_SESSION['userId'])){
+        $userId = $_SESSION['userId'];
 
-    if($deleteQueryResult){
-        header('Location: cart.php');
+        $deleteQuery = "
+            DELETE cd.* FROM cart_details cd
+            INNER JOIN cart c ON c.cart_id = cd.cart_id
+            INNER JOIN users u ON u.user_id = c.user_id
+            WHERE u.user_id=$userId AND cd.product_id=$productId;
+        ";
+        $deleteQueryResult = mysqli_query($connection, $deleteQuery);
+    
+        if($deleteQueryResult){
+            header('Location: cart.php');
+        }
+    }
+    else{
+        // print_r($_SESSION['currentCart']);
+        for($i=0;$i<sizeof($_SESSION['currentCart']);$i++){
+            foreach($_SESSION['currentCart'][$i] as $cartProductId=>$cartProductQuantity){
+                if($cartProductId==$productId){
+                    // unset($_SESSION['currentCart'][$i]);
+                    array_splice($_SESSION['currentCart'],$i,1);
+                    header('Location: cart.php');
+                }
+            }
+        }
+
+        // foreach($_SESSION['currentCart'] )
     }
 
 ?>
